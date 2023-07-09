227f58014f19adaec8a8c152dd2617ec6870bc8e
mcnedward <edwardmcnealy@gmail.com>
2023-07-09 16:44:29 -0400

    Move sort table to common component

---
+++ src/client/common/components/SortTable.tsx
@@ -0,0 +1,161 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+
+export type Order = 'asc' | 'desc';
+
+export interface HeadCell<T> {
+  id: keyof T;
+  label: string;
+  numeric: boolean;
+  disablePadding: boolean;
+  sortable?: boolean;
+}
+
+interface EnhancedTableProps<T> {
+  headCells: HeadCell<T>[];
+  order: Order;
+  orderBy: keyof T;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof T) => void;
+}
+
+function EnhancedTableHead<T>(props: EnhancedTableProps<T>) {
+  const { headCells, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property: keyof T) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id.toString()}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+              disabled={!headCell.sortable}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box
+                  component="span"
+                  sx={{
+                    border: 0,
+                    clip: 'rect(0 0 0 0)',
+                    height: '1px',
+                    margin: -1,
+                    overflow: 'hidden',
+                    padding: 0,
+                    position: 'absolute',
+                    whiteSpace: 'nowrap',
+                    width: '1px',
+                  }}
+                >
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface SortTableProps<T> {
+  data: T[];
+  total: number;
+  headCells: HeadCell<T>[];
+  initialOrderBy: keyof T;
+  onPagePropsChange: (page: number, rowsPerPage: number, order: Order, orderBy: keyof T) => void;
+  children: (item: T) => React.ReactNode;
+}
+
+export default function SortTable<T>({
+  data,
+  total,
+  headCells,
+  initialOrderBy,
+  onPagePropsChange,
+  children,
+}: SortTableProps<T>) {
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof T>(initialOrderBy);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof T) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = async (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const rows = parseInt(event.target.value, 10);
+    setRowsPerPage(rows);
+    setPage(0);
+  };
+
+  React.useEffect(() => {
+    onPagePropsChange(page, rowsPerPage, order, orderBy);
+  }, [page, rowsPerPage, order, orderBy]);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - total) : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={'medium'}>
+            <EnhancedTableHead
+              headCells={headCells}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {data.map((row, index) => children(row))}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={total}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
